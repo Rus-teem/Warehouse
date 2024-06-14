4926a57cc43fd18f878804e91175e4427a8762b8
Rus-teem <hv.rustem@gmail.com>
2024-06-14 18:20:19 +0300

    Ololo

---
+++ JS/Change_css.js
@@ -0,0 +1,7 @@
+// обращаемся к классу через селектор 
+let elem = document.querySelector(".t1000__background-image");
+// переписываем значение css 
+elem.style.cssText = 'border: 1px solid black'
+// ололо :)
+elem.appendChild(appendChild);
+/html/body / div[1] / div[4] / div / div / div[1] / div[1] / div / styleC:\Projects\Warehouse\JS\Change_css.js
