c7309a88785171485c65d96b2c5516b703456bec
Rus-teem <hv.rustem@gmail.com>
2024-06-17 12:40:02 +0300

    test

--- JS/Change_css.js
+++ JS/Change_css.js
@@ -4,4 +4,4 @@ let elem = document.querySelector(".t1000__background-image");
 elem.style.cssText = 'border: 1px solid black'
 // ололо :)
 elem.appendChild(appendChild);
-/html/body / div[1] / div[4] / div / div / div[1] / div[1] / div / styleC:\Projects\Warehouse\JS\Change_css.js
+/html/body / div[1] / div[4] / div / div / div[1] / div[1] / div / styleC: \Projects\Warehouse\JS\Change_css.js
