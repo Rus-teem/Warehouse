110d520fcbdb30f0ab38ce28af62b77cf0749a2c
Rustem <66059962+Rus-teem@users.noreply.github.com>
2024-06-21 01:51:10 +0300

    Извлекаем гет параметры

---
+++ JS/Extract_the_get_parameters.js
@@ -0,0 +1,21 @@
+// Пример получения значения url 
+const currentUrl = window.location.href;
+console.log(currentUrl); // => https://ru.hexlet.io/qna
+
+let url = new URL("http://www.example.com/page.html?param1=value1&amp;param2=value2&amp;param3=value3");
+let params = new URLSearchParams(url.search);
+ 
+let param3 = params.get('param3'); // "value3"
+
+
+let url = "http://www.example.com/page.html?param1=value1&amp;param2=value2&amp;param3=value3";
+//разьиваем значение
+let params = url.split('?')[1].split('&amp;'); 
+let param3;
+ 
+for(let i = 0; i &lt; params.length; i++) {
+    let pair = params[i].split('=');
+    if(pair[0] == 'param3') {
+        param3 = pair[1];
+    }
+}
