0c32b8e7b4e0ff8f8dbecef0130495f79f3609a8
Rustem <66059962+Rus-teem@users.noreply.github.com>
2024-06-23 18:43:46 +0300

    Погрались

---
+++ Python/Learn/Play_with_python--D.py
@@ -0,0 +1,13 @@
+# просто играюсь с питоном :D)))
+text = "hello"
+# количетсво символовd
+# input_text = input()
+txt2 = 5 == int(len('hello'))
+txt3 = len(str(str(45)+'hi'))
+txt4 = 4 == 4 and 4 != 5 or 'tex'=='tex'
+def hello (name, name2) :
+    print ('Привет !' + name + name2)
+    print ('Привет !!!')
+    print ('Привет всем.'+ name)
+hello(' python', str(54 + 3.124))
+print (text, txt2, txt3, txt3+txt2, txt4)
