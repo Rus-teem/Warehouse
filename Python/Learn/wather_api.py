9e8b59f7852fdf659ed1ffc1b40a22842d5e436f
Rus-teem <hv.rustem@gmail.com>
2024-07-05 16:04:04 +0300

    upd

--- Python/Learn/wather_api.py
+++ Python/Learn/wather_api.py
@@ -1,21 +1,29 @@
 # подключаем библиотеку для работы с запросами
 import requests
+import json
 
 # указываем город
 city = "Казань"
+cityId = 551487
+appId = "79d1ca96933b0328e1c7e3e7a26cb347"
 # формируем запрос
 url = (
     "https://api.openweathermap.org/data/2.5/weather?q="
     + city
-    + "&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347"
+    + "&units=metric&lang=ru&appid="
+    + appId
 )
 # отправляем запрос на сервер и сразу получаем результат
 weather_data = requests.get(url).json()
 data_requests = dict(weather_data)
+# print(data_requests)
 # получаем данные о температуре и о том, как она ощущается
 temperature = round(weather_data["main"]["temp"])
 temperature_feels = round(weather_data["main"]["feels_like"])
+weatherSky = data_requests["weather"]
+weatherSky2 = weatherSky[0]["description"]
 # выводим значения на экран
 print("Сейчас в городе", city, str(temperature), "°C")
 print("Ощущается как", str(temperature_feels), "°C")
-# print(data_requests)
+print("Состояние неба:", str(weatherSky2))
+# print(weather_data)
