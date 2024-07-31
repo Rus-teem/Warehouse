879d07c83418160fe959cfa3175a34b1f8a1572b
Rus-teem <hv.rustem@gmail.com>
2024-07-31 17:14:30 +0300

    UPD

--- JS/Change_css.js
+++ JS/Change_css.js
@@ -1,3 +1,7 @@
+
+// Обращение к элементу по id
+let elemId = document.getElementById('youtubeiframe764123363')
+
 // обращаемся к классу через селектор 
 let elem = document.querySelector(".t1000__background-image");
 // переписываем значение css 
@@ -5,3 +9,13 @@ elem.style.cssText = 'border: 1px solid black'
 // ололо :)
 elem.appendChild(appendChild);
 /html/body / div[1] / div[4] / div / div / div[1] / div[1] / div / styleC: \Projects\Warehouse\JS\Change_css.js
+
+< script >
+    window.addEventListener("DOMContentLoaded", function () {
+        let elemVideo = document.querySelector('.t-video-lazyload')
+        let elemId = document.querySelector('.t-video-lazyload>iframe')
+        elemId.style.cssText = 'border: 1px solid black'
+        elemId.style.cssText = 'border-radius: 30px'
+        elemVideo.style.cssText = 'border-radius: 30px'
+    }, false);
+</script >
