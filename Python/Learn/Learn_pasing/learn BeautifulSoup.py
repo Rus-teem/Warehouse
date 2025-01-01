b5c6539f2254d32d0870035e88186220f5c16878
Rustem <66059962+Rus-teem@users.noreply.github.com>
2025-01-02 02:18:46 +0300

    learn bs4

---
+++ Python/Learn/Learn_pasing/learn BeautifulSoup.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+
+import lxml
+
+html = """
+<html>
+  <body>
+    <div class="product">
+      <h2 class="product-name">Телефон</h2>
+      <span class="product-price">30,000 руб</span>
+      <a href="/product/phone" class="product-link">Подробнее</a>
+    </div>
+    <div class="product">
+      <h2 class="product-name">Ноутбук</h2>
+      <span class="product-price">80,000 руб</span>
+      <a href="/product/laptop" class="product-link">Подробнее</a>
+    </div>
+    <div class="product">
+      <h2 class="product-name">Часы</h2>
+      <span class="product-price">10,000 руб</span>
+      <a href="/product/watch" class="product-link">Подробнее</a>
+    </div>
+  </body>
+</html>
+
+"""
+
+# Создаём объект BeautifulSoup
+soup = BeautifulSoup(html, 'lxml')
+
+# Выводим красиво отформатированный HTML
+# print(soup.prettify())
+
+first_product = (soup.find('h2', class_='product-name')).text
+first_product_price = (soup.find('span', class_='product-price')).text
+first_product_link = soup.find('a', 'href')
+# first_product_link_text = first_product_link.get('href')
+
+# print("Продукт:", first_product, 'Цена:', first_product_price, "Ссылка:", first_product_link_text)
+print(first_product_link)
+
+all_product_name = soup.find_all('h2', class_='product-name')
+all_product_price = soup.find_all('span', class_='product-price')
+all_product_link = soup.find_all('a')
+
+
+name_product = []
+product_price = []
+product_links = []
+
+for links in all_product_link:
+    product_links_href = links.get('href')
+    product_links.append(product_links_href)
+
+for price in all_product_price:
+    product_name_list = price.text
+    product_price.append(product_name_list)
+    # print(product_price)
+
+for name in all_product_name:
+    product_name_list= name.text
+    name_product.append(product_name_list)
+    # print(name_product)
+
+print("название", name_product, "цена", product_price, "ссылка", product_links)
+
+
+for name, price, link in zip (name_product, product_price, product_links):
+    print(f"название товара - {name}, цена товара- {price}, ссылка товара - {link}")
+
+
+
+# for soups in soup:
+#     soups1 = (soup.find_all('h2', class_='product-name')).text
+#     soups2 = (soup.find_all('span', class_='product-price')).text
+#     first_product_link = soup.find('a')
+#     soups3 = first_product_link.get('href')
+#     print (soups1, soups2, soups3)
--- Python/Learn/Learn_pasing/step_parsing.py
+++ Python/Learn/Learn_pasing/step_parsing.py
@@ -1,32 +1,54 @@
-# pip install requests beautifulsoup4 - устанавливаем библиотеки 
+# pip install requests beautifulsoup4 - устанавливаем библиотеки
 
+# импорт бииблитеки bs4 
+from bs4 import BeautifulSoup 
 
+# импорт библиотеки bs4 
 import requests
 
-# указываем в переменной url с которого мы будем забирать данные 
+# указываем в переменной url с которого мы будем забирать данные
 url = "https://2gis.ru/kazan/search/%D1%81%D1%82%D0%BE%D0%BC%D0%B0%D1%82%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D1%8F"
 
 # отправляем фейковый user agent
-headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
+headers = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
 
+# Устанавливаем время тайм аута в запросе
+time_out_req = 5
 # Оправляем запрос по урлу с содержимым user agent
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers, timeout=time_out_req)
+
+
+# Получаем данные непосредственно код ответа
+status_code_answer = response.status_code
+print(status_code_answer)
 
 # проверка содержимого ответа черех другую переменную
 status = response
-print(f"Тут содержимое response: {status}")
+print(f"Тут содержимое response: {status}",
+      f"а тут непосредствено код - {status_code_answer}")
 
-print(response.status_code)
+# по сути получаю ответ тела HTML страницы
+answerStroke = response.text
 
-# response = requests.get("https://jsonplaceholder.typicode.com/posts", params={"userId": 1})
-# print(response.status_code)  # Статус-код ответа (например, 200)
-# # print(response.json())       # Преобразование JSON-ответа в Python-объект
+# записываю ответ в корень в формате .txt
+with open("htmlAnswer2gis.txt", mode="w", encoding="utf-8") as file:
+    file.write(answerStroke)
 
-# Проверяем статус-код ответа
-# if response.status_code == 200:
-#     print("Страница успешно загружена!")
-# else:
-#     print(f"Ошибка загрузки: {response.status_code}")
+# Проверяем статус ответа
+if response.status_code == 200:
+    # Создаём объект BeautifulSoup из текста ответа
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Дальнейшая работа с soup
+    print(soup.title.text)  # Выводим заголовок страницы
+else:
+    print(f"Ошибка при запросе: {response.status_code}")
+
+print("текст заголовка", soup.title.text)
 
 
+# items = soup.find_all(class_="_1cd6avd")
+# title = items.soup.find('span')
+# print(title)
 
